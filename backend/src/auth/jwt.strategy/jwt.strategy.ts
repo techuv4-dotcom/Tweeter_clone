f2faf65656f1c0f53cd01fcf54cec1cb30e16560
mohd uvaish <techuv@gmail.com>
2026-06-14 13:12:46 +0530

    fix: update JWT secret key to use environment variable

--- backend/src/auth/jwt.strategy/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secrateKey',
+      secretOrKey:process.env.JWT_SECRET_KEY!,
     });
   }
 
