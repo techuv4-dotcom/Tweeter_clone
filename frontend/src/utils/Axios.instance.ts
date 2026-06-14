74587178fa371c6cf2b72b9d613e35c21abadf18
mohd uvaish <techuv@gmail.com>
2026-06-14 17:26:34 +0530

    add env varable

--- frontend/src/utils/Axios.instance.ts
+++ frontend/src/utils/Axios.instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://backend-tweeter-clone.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL,
     headers:{
         "Content-Type":"application/json"
     }
